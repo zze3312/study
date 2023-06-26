052ba9d0326869a08cf0c586c40af504b0dea3be
zze3312 <jihyeon1910@gmail.com>
2023-06-26 10:09:41 +0900

    comma test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-   <h1>HelloWorld</h1>
-  );
+  return <h1>HelloWorld</h1>;
 }
 
 export default App;
+
+let test1 = "1";
+let test2 = "2";
+let value = "^";
+
+function fncTest(val) {
+  let number = "3";
+  return val + "테스트입니다" + number;
+}
+
+console.log(
+  'test1의 값은 : "' +
+    fncTest(test1) +
+    '" / test2의 값은 : ' +
+    fncTest("222222"),
+);
