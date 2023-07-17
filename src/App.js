467e18f0cfbb617f22e35b017d10dc75833735e3
zze3312 <jihyeon1910@gmail.com>
2023-07-17 16:30:17 +0900

    bootstrap추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 import "./javaScriptPrctice/part1/4.map_collection";
 
 function App() {
-  return <h1>HelloWorld</h1>;
+  return (
+    <div className="container text-center">
+      <div className="row h-50">
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+        <div className="col">Column</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
